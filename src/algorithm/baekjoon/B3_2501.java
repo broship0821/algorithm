87d1b396c60bdbaca2e32aa280aadf812fca546b
peter <gudqo1995@naver.com>
2021-05-01 15:04:50 +0900

    B3_2501

---
+++ src/algorithm/baekjoon/B3_2501.java
@@ -0,0 +1,27 @@
+package algorithm.baekjoon;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class B3_2501 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if(n%i==0){
+                list.add(i);
+            }
+        }
+
+        if (list.size()>=k)
+            System.out.println(list.get(k-1));
+        else
+            System.out.println(0);
+
+    }
+}
--- src/algorithm/baekjoon/S3_1283.java
+++ src/algorithm/baekjoon/S3_1283.java
@@ -13,13 +13,14 @@ public class S3_1283 {
         for (int i = 0; i < n; i++) {
             options[i] = sc.nextLine();
         }
-
+        //단축키 저장하는 set
         Set<Character> keys = new HashSet<>();
 
         for (int i = 0; i < n; i++) {
             String[] tmp = options[i].split(" ");
             int j = 0;
             int size = tmp.length;
+            //각 단어 첫번째 글자 단축키 지정되있는지 체크, 없을경우 추가
             for (; j < size; j++) {
                 Character key = Character.toUpperCase(tmp[j].charAt(0));
                 if(!keys.contains(key)){
@@ -27,7 +28,7 @@ public class S3_1283 {
                     break;
                 }
             }
-            if(j==size){
+            if(j==size){//각 단어 첫번째 글자가 전부 사용중일때 모든 글자 하나씩 체크
                 for (int m = 0; m < size; m++) {
                     boolean flag = false;
                     for (int k = 0; k < tmp[m].length(); k++) {
@@ -42,7 +43,7 @@ public class S3_1283 {
                 }
             }
         }
-
+        //저장된 단축키 출력
         System.out.println(keys);
     }
 }
