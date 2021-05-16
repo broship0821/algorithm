a7ec5b3da9d0262f44e22bfb6571c1abb14918af
peter <gudqo1995@naver.com>
2021-05-17 08:39:51 +0900

    S4_1978

--- src/algorithm/Main.java
+++ src/algorithm/Main.java
@@ -3,6 +3,9 @@ package algorithm;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println(3/2);
+        System.out.println(3%2);
+        System.out.println(3/3);
+        System.out.println(3%3);
     }
 }
--- src/algorithm/baekjoon/S4_1978.java
+++ src/algorithm/baekjoon/S4_1978.java
@@ -1,18 +1,19 @@
 package algorithm.baekjoon;
 
 import java.util.Scanner;
-//틀림 어디서 틀렸을까?
+
 public class S4_1978 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        //배열 입력 받기
         int[] nums = new int[n];
         for (int i = 0; i < n; i++) {
             nums[i] = sc.nextInt();
         }
-
+        //소수 개수 담을 변수
         int cnt = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {//배열을 하나씩 돌면서 소수인지 판별한다
             int j = 2;
             for (; j < nums[i]; j++) {
                 if (nums[i] % j == 0)
@@ -20,7 +21,6 @@ public class S4_1978 {
             }
             if (j == nums[i]){
                 cnt++;
-                System.out.println(nums[i]);
             }
         }
         System.out.println(cnt);
