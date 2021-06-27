b36ac912b2e38a468925d8ee800a0c16da969a5f
peter <gudqo1995@naver.com>
2021-06-27 14:13:22 +0900

    B2609 B1934

--- src/algorithm/baekjoon/B2609.java
+++ src/algorithm/baekjoon/B2609.java
@@ -3,20 +3,23 @@ package algorithm.baekjoon;
 import java.util.Scanner;
 //최대공약수와 최소공배수
 public class B2609 {
+
+    //최대공약수 재귀 방식
+    public static int gcd(int a, int b){
+        if (b==0) return a;
+        //gcd(a,b) = gcd(b,r) 이므로 r을 a%b로 바꿔줌
+        return gcd(b, a%b);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
 
         //최대공약수
-        int c = 0;
-        for (int i = 1; i <= Math.max(a, b); i++) {
-            if (a%i==0 && b%i==0)
-                c = i;
-        }
-        System.out.println(c);
+        int c = gcd(a, b);
 
-        //최소공배수
-        
+        System.out.println(c);
+        System.out.println(a * b / c);//최소공배수
     }
 }
