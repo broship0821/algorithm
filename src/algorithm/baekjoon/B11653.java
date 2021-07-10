984d773fedb3d620f78ddb8125d52f2aaaee844e
peter <gudqo1995@naver.com>
2021-07-10 16:12:11 +0900

    B11653

---
+++ src/algorithm/baekjoon/B11653.java
@@ -0,0 +1,30 @@
+package algorithm.baekjoon;
+
+import java.util.Scanner;
+
+public class B11653 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+//        int idx = 2;
+//        while (n!=1){
+//            if (n%idx==0){
+//                System.out.println(idx);
+//                n /= idx;
+//                continue;
+//            }
+//            idx++;
+//        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            while (n%i == 0){
+                System.out.println(i);
+                n /= i;
+            }
+        }
+
+        if (n != 1)
+            System.out.println(n);
+    }
+}
