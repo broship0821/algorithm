15fe8ccb1cde003adc8e78f1e2e808456151040c
peter <gudqo1995@naver.com>
2021-08-01 18:53:58 +0900

    B14002

---
+++ src/algorithm/baekjoon/dynamic/B14002.java
@@ -0,0 +1,27 @@
+package algorithm.baekjoon.dynamic;
+
+import java.util.*;
+
+public class B14002 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+
+                }
+            }
+        }
+        //이제 역추적만 하면 되는데 방법을 모르겠다다
+    }
+}
