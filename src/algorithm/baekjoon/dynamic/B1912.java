e81973db195c6c842b204ac72397b2d17e25ffa6
peter <gudqo1995@naver.com>
2021-08-03 09:10:02 +0900

    B1912

---
+++ src/algorithm/baekjoon/dynamic/B1912.java
@@ -0,0 +1,23 @@
+package algorithm.baekjoon.dynamic;
+
+import java.util.Scanner;
+
+public class B1912 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] dp = new int[n];
+        int max = dp[0] = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i-1] + arr[i], arr[i]);
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+    }
+}
