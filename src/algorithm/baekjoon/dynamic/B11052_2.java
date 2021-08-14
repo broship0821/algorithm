b20f5b68cc4ddd5e7823737c2ece6d74063787fa
peter <gudqo1995@naver.com>
2021-08-14 10:43:05 +0900

    B11052_2

--- src/algorithm/baekjoon/dynamic/B11052_2.java
+++ src/algorithm/baekjoon/dynamic/B11052_2.java
@@ -10,8 +10,8 @@ public class B11052_2 {
         for (int i = 1; i <= n; i++) {
             //i개가 들은 팩을 살때
             dp[i] = sc.nextInt();
-            //반복문을 돌며 dp[i-j] + dp[j] 의 값중 가장 큰 값을 dp에 저장한다
-            for (int j = 1; j <= i; j++) {
+            //반복문을 돌며 모든 dp[i-j] + dp[j] 의 값중 가장 큰 값을 dp에 저장한다
+            for (int j = 1; j <= i/2; j++) {
                 dp[i] = Math.max(dp[i], dp[i-j] + dp[j]);
             }
         }
