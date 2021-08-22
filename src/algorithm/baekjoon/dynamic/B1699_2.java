4765a1b7e31961264b62603d2d75b718be181efb
peter <gudqo1995@naver.com>
2021-08-23 07:57:30 +0900

    B1912_2

---
+++ src/algorithm/baekjoon/dynamic/B1699_2.java
@@ -0,0 +1,11 @@
+package algorithm.baekjoon.dynamic;
+
+import java.util.Scanner;
+
+public class B1699_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println((int) Math.sqrt(n));
+    }
+}
--- src/algorithm/baekjoon/dynamic/B1912_2.java
+++ src/algorithm/baekjoon/dynamic/B1912_2.java
@@ -2,7 +2,7 @@ package algorithm.baekjoon.dynamic;
 
 import java.util.Scanner;
 
-public class B1912 {
+public class B1912_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -13,12 +13,16 @@ public class B1912 {
 
         int[] dp = new int[n];
         // 맨 첫번째 숫자의 연속합은 자기 자신
-        int max = dp[0] = arr[0];
+        dp[0] = arr[0];
         //dp 배열에 i번째 연속합 중 가장 큰 값을 넣는다
         for (int i = 1; i < n; i++) {
             //i-1까지의 연속합 + i번째 숫자 or i번째 숫자 중 가장 큰 값이 dp[i]
-            dp[i] = Math.max(dp[i-1] + arr[i], arr[i]);
-            //dp 배열 중 가장 큰 값을 기록해둔다(연속합 중 가장 큰 값)
+            dp[i] = Math.max(arr[i], dp[i-1] + arr[i]);
+        }
+
+        //가장 큰 연속합 찾기
+        int max = dp[0];
+        for (int i = 1; i < n; i++) {
             max = Math.max(max, dp[i]);
         }
         System.out.println(max);
