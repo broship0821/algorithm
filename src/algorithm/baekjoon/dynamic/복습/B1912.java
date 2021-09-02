1ad21295197990ced2896e3a0cda461986602b96
peter <gudqo1995@naver.com>
2021-09-02 09:27:49 +0900

    dp 복습

---
+++ src/algorithm/baekjoon/dynamic/복습/B1912.java
@@ -0,0 +1,15 @@
+package algorithm.baekjoon.dynamic.복습;
+
+import java.util.Scanner;
+
+public class B1912 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] dp = new int[n];
+    }
+}
