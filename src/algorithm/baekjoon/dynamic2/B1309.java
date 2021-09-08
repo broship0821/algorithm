08e3680d9f7509e0b5d1438625a9cbdf0db7f2cc
peter <gudqo1995@naver.com>
2021-09-09 08:15:15 +0900

    B1309

---
+++ src/algorithm/baekjoon/dynamic2/B1309.java
@@ -0,0 +1,12 @@
+package algorithm.baekjoon.dynamic2;
+
+import java.util.Scanner;
+
+public class B1309 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] dp = new int[n+1][n+1];
+
+    }
+}
